15ddfd56506b3041de88dc413e9bafe23d721f4f
Brickcaster <dralon@yahoo.com>
2018-10-20 09:34:41 -0400

    More standalone bugfixes

--- modules/divOresity.lua
+++ modules/divOresity.lua
@@ -32,9 +32,9 @@ function diversify(event)
 		if math.abs(v.position.x) > EXEMPT_AREA or math.abs(v.position.y) > EXEMPT_AREA then
 			if v.prototype.resource_category == "basic-solid" then
 				local random = math.random()
-				if v.name == "stone" and global.STONE_BYPRODUCT then
+				if v.name == "stone" and STONE_BYPRODUCT then
 					v.destroy()
-				elseif random < global.DIVERSITY_QUOTA then --Replace!
+				elseif random < DIVERSITY_QUOTA then --Replace!
 					local refugee
 					if v.prototype.mineable_properties.required_fluid and EXTRA_DIVORESITY then
 						refugee = global.extra_diverse_ores[math.random(#global.extra_diverse_ores)]
@@ -43,7 +43,7 @@ function diversify(event)
 					end
 					event.surface.create_entity{name=refugee, position=v.position, amount=v.amount}
 					v.destroy()
-				elseif global.STONE_BYPRODUCT and random < global.STONE_BYPRODUCT_RATIO then --Replace with stone!
+				elseif STONE_BYPRODUCT and random < STONE_BYPRODUCT_RATIO then --Replace with stone!
 					event.surface.create_entity{name="stone", position=v.position, amount=v.amount}
 					v.destroy()
 				end
