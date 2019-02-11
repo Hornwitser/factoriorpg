5cce2c09a6a4407a6b1f9253eb2a0bd22df4b634
Brickcaster <dralon@yahoo.com>
2019-02-10 21:55:41 -0500

    DivOresity
    Function needed surface name, not the surface.

--- modules/divOresity.lua
+++ modules/divOresity.lua
@@ -157,7 +157,7 @@ function diversify(event)
 					else
 						refugee = get_type(global.diverse_ores)
 					end
-					correction_factor = richness_correction_factor(event.surface, v.name, refugee)
+					correction_factor = richness_correction_factor(event.surface.name, v.name, refugee)
 					event.surface.create_entity{name=refugee, position=v.position, amount=v.amount * correction_factor}
 					v.destroy()
 				elseif STONE_BYPRODUCT and random < STONE_BYPRODUCT_RATIO then --Replace with stone!
